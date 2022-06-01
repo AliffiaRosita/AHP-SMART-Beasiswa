a61842396348a5a1af28f017169562330876065f
Aliffia Rosita <aliffia.rosita@gmail.com>
2022-06-01 10:31:37 +0800

    add template and data table

---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>General Dashboard &mdash; Stisla</title>
+
+ @include('template.style')
+</head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+      <nav class="navbar navbar-expand-lg main-navbar">
+        @include('template.topnav')
+      </nav>
+
+      <div class="main-sidebar">
+        @include('template.sidebar')
+      </div>
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>{{$title??''}}</h1>
+          </div>
+         
+            @yield('content')
+        </section>
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+        </div>
+        <div class="footer-right">
+          2.3.0
+        </div>
+      </footer>
+    </div>
+  </div>
+
+  @include('template.script')
+</body>
+</html>
