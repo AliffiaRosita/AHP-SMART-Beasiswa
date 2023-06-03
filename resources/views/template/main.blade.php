000ebbb4726e19a5ef9e0cc172373dd8a90b126a
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-06-03 14:04:21 +0800

    puter

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -22,15 +22,12 @@
 
       <!-- Main Content -->
       <div class="main-content">
-        
+
         @yield('content')
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
-        </div>
-        <div class="footer-right">
-          2.3.0
+            <a href="#">FARIDA PURNOMO</a>
         </div>
       </footer>
     </div>
